abd9cd1d2df3206be9cb6011d20b6a2da7b468c4
Likitha Adapa <adapa.likitha2004@gmail.com>
2026-01-22 12:22:17 +0530

    code

---
+++ variables/main.js
@@ -0,0 +1,53 @@
+let a ="Ramya"
+let b ="Sai"
+let c ="ramya's"
+let d ='ramya"s'
+let e ="ramya\'s"
+let g ="he said that \"he will come tomorrow\" "
+let h =`hi how r u "hlo"`
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(g);
+console.log(h);
+
+let welcome="Hello,"
+let name="Lally"
+let email="lally1@gmail.com"
+let phone="9863548768"
+console.log(welcome+"i am "+name+".My email address is "+email+" and phone number is "+phone);
+let display=welcome+"i am "+name+".My email address is "+email+" and phone number is "+phone
+console.log(display);
+
+let i='33'
+let j=44
+let k=(i+j)
+console.log(k);
+console.log(typeof k);
+
+let z=((0.1*10)+(0.2*10))/10
+console.log(z);
+
+let x=43
+x +=4
+console.log(x);
+
+let name1='Likitha'
+let from='Pregnapur'
+let clg='VBIT'
+let rollno='22P61A6603'
+console.log("I am "+name1+",I am from "+from+",I'm studying in "+clg+" college,My rollno is "+rollno);
+console.log(`I am ${name1},I am from ${from},I'm studying in ${clg} college,My rollno is ${rollno}`);
+
+console.log(`* * * * *
+  * * * *
+    * * *
+      * * 
+        * `);
+  let a1="65"
+  let b1=65
+  console.log(a1==b1);
+  console.log(a1===b1);
+  
